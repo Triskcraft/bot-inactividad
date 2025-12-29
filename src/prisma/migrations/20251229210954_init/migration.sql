607d6f05c7594a3e7fd396c2343bbff712e00542
William Corea Ledezma <w.corea.1@estudiantec.cr>
2025-12-29 15:10:46 -0600

    migracion de sqlite a postgre
    Co-authored-by: Elí Maciel <tecno_eli@hotmail.com>

--- src/prisma/migrations/20251229210954_init/migration.sql
+++ src/prisma/migrations/20251229210954_init/migration.sql
@@ -1,49 +1,54 @@
--- ! added after generated
-PRAGMA journal_mode = WAL;
-
 -- CreateTable
 CREATE TABLE "inactivity_periods" (
-    "user_id" TEXT NOT NULL PRIMARY KEY,
+    "user_id" TEXT NOT NULL,
     "guild_id" TEXT NOT NULL,
     "role_snapshot" TEXT NOT NULL,
-    "started_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "ends_at" DATETIME NOT NULL,
+    "started_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "ends_at" TIMESTAMP(3) NOT NULL,
     "source" TEXT NOT NULL,
-    "notified" BOOLEAN NOT NULL
+    "notified" BOOLEAN NOT NULL,
+
+    CONSTRAINT "inactivity_periods_pkey" PRIMARY KEY ("user_id")
 );
 
 -- CreateTable
 CREATE TABLE "tracked_roles" (
     "guild_id" TEXT NOT NULL,
     "role_id" TEXT NOT NULL,
-    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    PRIMARY KEY ("guild_id", "role_id")
+    CONSTRAINT "tracked_roles_pkey" PRIMARY KEY ("guild_id","role_id")
 );
 
 -- CreateTable
 CREATE TABLE "role_statistics" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "guild_id" TEXT NOT NULL,
     "role_id" TEXT NOT NULL,
     "inactive_count" INTEGER NOT NULL,
     "active_count" INTEGER NOT NULL,
-    "captured_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "captured_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "role_statistics_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "links" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "discord_id" TEXT NOT NULL,
-    "mc_id" TEXT NOT NULL
+    "mc_id" TEXT NOT NULL,
+
+    CONSTRAINT "links_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "link_codes" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL,
     "discord_id" TEXT NOT NULL,
     "discord_nickname" TEXT NOT NULL,
-    "code" TEXT NOT NULL
+    "code" TEXT NOT NULL,
+
+    CONSTRAINT "link_codes_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
