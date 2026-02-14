f99e4f72c25b5d41385e14e79c6e5fca10cb15fa
Elí Maciel <tecno_eli@hotmail.com>
2026-02-14 15:09:29 -0600

    logger pretty

--- src/logger.ts
+++ src/logger.ts
@@ -1,19 +1,11 @@
 import pino from 'pino'
-import { envs } from './config.ts'
 
-/**
- * Configuración centralizada de logging. Se usa Pino por su alto rendimiento y
- * compatibilidad con transporte "pretty" en desarrollo.
- */
 const pinoConfig: Parameters<typeof pino>[0] = {
     level: process.env.LOG_LEVEL ?? 'info',
-}
-
-if (envs.NODE_ENV === 'development') {
-    pinoConfig.transport = {
+    transport: {
         target: 'pino-pretty',
         options: { colorize: true, translateTime: 'SYS:standard' },
-    }
+    },
 }
 
 export const logger = pino(pinoConfig)
